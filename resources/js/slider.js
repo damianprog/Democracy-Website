ef496822616ce3d909f12da1ac244200ad2b2d34
damianprog <damianpukszta@gmail.com>
2020-10-06 15:22:23 +0200

    Slider interval created

--- resources/js/slider.js
+++ resources/js/slider.js
@@ -1,18 +1,34 @@
 (() => {
-    const blogPosts = document.querySelector(".blog-posts-slider");
+    const blogPostsSlider = document.querySelector(".blog-posts-slider");
     const prevBtn = document.querySelector(".prev-btn");
     const nextBtn = document.querySelector(".next-btn");
 
+    const startSliding = () => {
+        const sliderStyle = getComputedStyle(blogPostsSlider);     
+        const leftValue = sliderStyle.getPropertyValue("left");
+
+        leftValue == "0px" ? next() : prev();
+    };
+
+    let slideInterval = setInterval(startSliding, 10000);
+
+    restartSlideInterval = () => {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(startSliding, 10000);
+    };
+
     prev = () => {
-        blogPosts.style.left = "0";
+        blogPostsSlider.style.left = "0";
         prevBtn.style.color = "#f26200";
         nextBtn.style.color = "#757575";
+        restartSlideInterval();
     }
 
     next = () => {
-        blogPosts.style.left = "-102%";
+        blogPostsSlider.style.left = "-102%";
         nextBtn.style.color = "#f26200";
         prevBtn.style.color = "#757575";
+        restartSlideInterval();
     }
 
     prevBtn.addEventListener("click", prev);
